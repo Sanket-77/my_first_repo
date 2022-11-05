4ccce58bfe7b9409167b5eb6a4391b862b52990e
sanket-77 <sanketchavan4686@gmail.com>
2022-11-05 18:41:48 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static <string> void main(String[] args){
+        String name1 = "sanket";
+        String name2 = "bittu;";
+        String name3 = name1  + " urf "  + name2;
+        System.out.println(name3);
+        int age = 25;
+        System.out.println(age);
+        System.out.println(name1.charAt(3));
+        System.out.println(name1.length());
+        System.out.println(name2.replace("b","B"));
